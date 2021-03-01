bc04fa7cb2e2d07cf9468d2c1c933640cc9a576f
bellhundred <good5229@naver.com>
2021-03-01 10:38:15 +0900

    feat: add screen with subjoin animal object

--- lib/sub/secondPage.dart
+++ lib/sub/secondPage.dart
@@ -1,19 +1,109 @@
 import 'package:flutter/material.dart';
 import '../animalItem.dart';
 
-class SecondApp extends StatelessWidget {
+class SecondApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SecondApp();
   final List<Animal> list;
 
   SecondApp({Key key, this.list}) : super(key: key);
+}
+
+class _SecondApp extends State<SecondApp> {
+  final nameController = TextEditingController();
+  int _radioValue = 0;
+  bool flyExist = false;
+  var _imagePath;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       body: Container(
         child: Center(
-          child: Text("두 번째 페이지"),
-        ),
-      ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                maxLines: 1,
+              ),
+              Row(
+                children: <Widget>[
+                  Radio(value:0, groupValue: _radioValue, onChanged: _radioChange),
+                  Text('양서류'),
+                  Radio(value:1, groupValue: _radioValue, onChanged: _radioChange),
+                  Text('파충류'),
+                  Radio(value:2, groupValue: _radioValue, onChanged: _radioChange),
+                  Text('포유류'),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Row(
+                children: <Widget>[
+                  Text('날 수 있나요?'),
+                  Checkbox(value: flyExist, onChanged: (check){
+                    setState(() {
+                      flyExist= check;
+                    });
+                  })
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Row(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Image.asset('repo/images/cow.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'repo/images/cow.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('repo/images/pig.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'repo/images/pig.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('repo/images/bee.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'repo/images/bee.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('repo/images/cat.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'repo/images/cat.png';
+                    },
+                  ),
+                  // GestureDetector(
+                  //   child: Image.asset('repo/images/fox.png', width: 80),
+                  //   onTap: () {
+                  //     _imagePath = 'repo/images/fox.png';
+                  //   },
+                  // ),
+                  // GestureDetector(
+                  //   child: Image.asset('repo/images/monkey.png', width: 80),
+                  //   onTap: () {
+                  //     _imagePath = 'repo/images/monkey.png';
+                  //   },
+                  // ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              RaisedButton(
+                  child: Text('동물 추가하기'),
+                  onPressed:(){}
+              )
+            ],
+          )
+        )
+      )
     );
+  }
+  _radioChange(int value){
+    setState(() {
+      _radioValue = value;
+    });
   }
 }
